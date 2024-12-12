1f2ef608a62ca045c3f9e09eac88566e22011810
Marko Berghoff <berghoffmj@gmail.com>
2024-12-12 13:15:36 +0100

    added linear option for playlist shape

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Rearrange a given playlist to flow along a certain value ("party_score", computed from "energy" and "danceablitiy" of the songs),
 type of flow is chosen by user: linear or sinusoidal
 
-KNN_song_recommender and Party_playlist explain the logic, demo.ipynb runs the robo_jockey module.
+KNN_song_recommender and Party_playlist explain the logic, demo.ipynb demonstrates the robo_jockey module.
--- robo_jockey.py
+++ robo_jockey.py
@@ -4,8 +4,7 @@ import numpy as np
 import joblib
 
 
-# load song library
-df = pd.read_csv("data/data_cleaned.csv")
+
 
 
 
@@ -14,6 +13,8 @@ df = pd.read_csv("data/data_cleaned.csv")
 def find_filler(song):
     # given a song (by its index), use previously trained KNN model to return the 23 (random choice) nearest neighbors
 
+    # load song library
+    df = pd.read_csv("data/data_cleaned.csv")
     # get song's data, preprocess
     x = df[df.index==song].drop(columns=['track_name','track_artist', 'track_album_name','track_album_release_date','duration_ms'])
     x_scaled = MinMaxScaler().fit_transform(x) # little bit of data leakage...
@@ -26,8 +27,9 @@ def find_filler(song):
 
 
 def party_score(df):
+    # add party_score variable to a given dataframe
 
-    # first load song library and add party_score variable
+    # first load song library and add party_score variable to it
     data = pd.read_csv("data/data_cleaned.csv")
     # party_score is define as follows: take two-dimensional variable (x,y)
     x,y = data.energy,data.danceability
@@ -46,68 +48,79 @@ def party_score(df):
 
 
 
-def generate_playlist(songs_df,step_size):
-    #
+def generate_playlist(songlist_df,step_size):
+    # compute party_score for songlist_df, then generate playlist according to user choice
 
     # load song library with party_score variable
     df = party_score(pd.read_csv("data/data_cleaned.csv"))
 
-    # compute party_score for songs_df
-    songs_df = party_score(songs_df)
+    # compute party_score for songlist_df
+    songs_df = party_score(songlist_df)
     track_list = songs_df[["party_score"]]
 
-    #extract two minima (will be start and end song) and remove from track list
-    min1_idx = track_list.idxmin()
-    start_song = track_list[track_list.index == min1_idx[0]]
-    track_list.drop(index = min1_idx[0], inplace=True)
-    min2_idx = track_list.idxmin()
-    end_song = track_list[track_list.index == min2_idx[0]]
-    track_list.drop(index = min2_idx[0], inplace=True)
+    # determine playlist shape
+    choice = input("Choose the vibe! Ebb-and-flow (1 / default) or The-sky-is-the-limit (2): ")
 
-    # create bins, depending on step_size
-    dividers = [np.array([x*np.pi/step_size,(x+1)*np.pi/step_size]) for x in range(step_size)]
-    bins = [(np.sin(x-np.pi/2)+1)/2 for x in dividers]
+    if int(choice) == 2: # linear case
 
-    # fill bins with songs according to their party_score
-    binned_songs = []
-    for bin in bins:
-        in_this_bin = []
-        for i,x in zip(track_list.index,track_list.party_score):
-            if bin[0] < x and x <= bin[1]:
-                in_this_bin.append(i)
-        binned_songs.append(in_this_bin)
+        # order songs_df by party_score (no fillers needed)
+        return songs_df.sort_values(by = "party_score")
 
-    # set nr of loops through binned_songs in order to put every song on the playlist
-    l = max([len(i) for i in binned_songs])
 
-    # successively add songs from binned_songs until all bins are empty, adding filler songs if necessary
-    used_fillers = set() # keep track of fillers
-    playlist = [start_song.index[0]] # initialise playlist with start_song
+    else: # sinusoidal case
 
-    for iteration in range(l):
-        for elt in binned_songs:
-            if not elt: # case no song in bin, fill with song that is close to previous one
-                filler_df = find_filler(playlist[-1])
-                filler_df = party_score(filler_df)
+        #extract two minima (will be start and end song) and remove from track list
+        min1_idx = track_list.idxmin()
+        start_song = track_list[track_list.index == min1_idx[0]]
+        track_list.drop(index = min1_idx[0], inplace=True)
+        min2_idx = track_list.idxmin()
+        end_song = track_list[track_list.index == min2_idx[0]]
+        track_list.drop(index = min2_idx[0], inplace=True)
 
-                # exclude already used filler songs
-                available_fillers = filler_df[~filler_df.index.isin(used_fillers)]
+        # create bins, depending on step_size
+        dividers = [np.array([x*np.pi/step_size,(x+1)*np.pi/step_size]) for x in range(step_size)]
+        bins = [(np.sin(x-np.pi/2)+1)/2 for x in dividers]
 
-                if not available_fillers.empty:
-                    # find the song with party_score closest to previous one
-                    prev_party_score = df.loc[playlist[-1], "party_score"]
-                    closest_index = available_fillers.party_score.sub(prev_party_score).abs().idxmin()
+        # fill bins with songs according to their party_score
+        binned_songs = []
+        for bin in bins:
+            in_this_bin = []
+            for i,x in zip(track_list.index,track_list.party_score):
+                if bin[0] < x and x <= bin[1]:
+                    in_this_bin.append(i)
+            binned_songs.append(in_this_bin)
 
-                    # add to playlist and mark as used
-                    playlist.append(closest_index)
-                    used_fillers.add(closest_index)
+        # set nr of loops through binned_songs in order to put every song on the playlist
+        l = max([len(i) for i in binned_songs])
+
+        # successively add songs from binned_songs until all bins are empty, adding filler songs if necessary
+        used_fillers = set() # keep track of fillers
+        playlist = [start_song.index[0]] # initialise playlist with start_song
+
+        for iteration in range(l):
+            for elt in binned_songs:
+                if not elt: # case no song in bin, fill with song that is close to previous one
+                    filler_df = find_filler(playlist[-1])
+                    filler_df = party_score(filler_df)
+
+                    # exclude already used filler songs
+                    available_fillers = filler_df[~filler_df.index.isin(used_fillers)]
+
+                    if not available_fillers.empty:
+                        # find the song with party_score closest to previous one
+                        prev_party_score = df.loc[playlist[-1], "party_score"]
+                        closest_index = available_fillers.party_score.sub(prev_party_score).abs().idxmin()
+
+                        # add to playlist and mark as used
+                        playlist.append(closest_index)
+                        used_fillers.add(closest_index)
+                    else:
+                        # no valid filler found
+                        print(f"No valid unique filler found.")
                 else:
-                    # no valid filler found
-                    print(f"No valid unique filler found.")
-            else:
-                playlist.append(elt[0])
-                elt.remove(elt[0])
+                    playlist.append(elt[0])
+                    elt.remove(elt[0])
 
-    playlist.append(end_song.index[0]) # add end_song
-
-    return playlist
+        playlist.append(end_song.index[0]) # add end_song
+        playlist_df = df.loc[playlist]
+        return playlist_df
